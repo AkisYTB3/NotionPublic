a56998280af1b44a169defc25bc6eada230a8554
Richard Mikloš <akisytbsteamy@gmail.com>
2025-02-26 12:10:11 +0100

    Initial Commit

---
+++ src/main/java/org/notionsmp/plugins/Notion.java
@@ -0,0 +1,16 @@
+package org.notionsmp.plugins;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Notion extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
