7a9048a451b851a6281b4eaf172cdf02bc684b9d
Richard Mikloš <akisytbsteamy@gmail.com>
2025-03-03 21:06:26 +0100

    refactor: improve /notion with a send method and an interactive help message

--- src/main/java/org/notionsmp/notion/commands/NotionCommand.java
+++ src/main/java/org/notionsmp/notion/commands/NotionCommand.java
@@ -13,9 +13,12 @@ public class NotionCommand extends BaseCommand {
     @Default
     @HelpCommand
     public void onHelp(CommandSender sender) {
-        sender.sendMessage(MINI_MESSAGE.deserialize("""
-                <#FFB022>┌────────────┤ NAME <#FFB022>├────────────┐
-                <#FFB022>├<#FFFF00>/notion <#FF8800>help <white> - shows this menu
-                """.replace("NAME", NAME)));
+        send(sender, "<#FFB022>┌────────────┤ NAME<#FFB022>├────────────┐".replace("NAME", NAME));
+        send(sender, "<#FFB022>├ <hover:show_text:'Click to insert...'><click:suggest_command:'/notion help'><#454545>/notion <#FFB022>help <white>- shows this menu</click></hover>");
+    }
+
+
+    private void send(CommandSender sender, String message) {
+        sender.sendMessage(MINI_MESSAGE.deserialize(message));
     }
 }
